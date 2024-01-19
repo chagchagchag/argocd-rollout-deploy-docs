475fcaf05de92795add365c42b30242e4f64316b
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-20 01:22:54 +0900

    2024.01

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ import { withDocus } from 'docus'
 
 // Learn more at https://docus.dev
 export default withDocus({
+  target: 'static',
+  router: {
+    base: '/argocd-rollout-deploy-docs/'
+  },
   docus: {
     // primaryColor: '#E24F55'
   }
