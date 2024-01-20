0dbd649aa8422d12cfbc51f93c980cab3016b4de
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-20 17:04:09 +0900

    디렉터리 구조 작업

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,11 @@
 echo ""
-echo "yarn install... "
+echo ">>> yarn install... "
 yarn install
 
 echo ""
-echo "yarn generate... (nuxt generate)"
+echo ">>> yarn generate... (nuxt generate)"
 yarn generate
 
 echo ""
-echo "yarn deploy (push-dir)"
+echo ">>> yarn deploy (push-dir)"
 yarn deploy
